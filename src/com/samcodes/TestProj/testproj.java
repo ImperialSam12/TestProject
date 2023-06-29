afbe1169bfd5f5544ad4d32f32c60671cf6d3771
SamHernandez <samdamangaming@gmail.com>
2023-06-28 20:05:42 -0700

    a commit v2

---
+++ src/com/samcodes/TestProj/testproj.java
@@ -0,0 +1,14 @@
+package com.samcodes.TestProj;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TestProj extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        onDisable();
+        return;
+    }
+    public void onDisable() {
+
+    }
+}
